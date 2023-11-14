8386c5241e5704266dda7d7e198e56e7191e0a95
yourboyha <w.wimonput@gmail.com>
2023-11-14 10:54:03 +0700

    aA

---
+++ pages/submit.php
@@ -0,0 +1,23 @@
+<?php
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $city = $_POST['city'];
+    $web = $_POST['web'];
+    $role = $_POST['role'];
+    $sign1 = $_POST['sign1'];
+    $sign2 = $_POST['sign2'];
+    $sign3 = $_POST['sign3'];
+
+
+    echo 'email: '.$email.'<br>';
+    echo 'password: '.$password.'<br>';
+    echo 'city: '.$city.'<br>';
+    echo 'web: '.$web.'<br>';
+    echo 'role: '.$role.'<br>';
+    echo 'sign1: '.$sign1.'<br>';
+    echo 'sign2: '.$sign2.'<br>';
+    echo 'sign3: '.$sign3.'<br>';
+
+
+
+?>
