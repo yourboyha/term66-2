46a54232a123d4ae072309ab672657a39dbbd83d
yourboyha <w.wimonput@gmail.com>
2023-11-14 09:46:22 +0700

    add variable.php

---
+++ Variable.php
@@ -0,0 +1,23 @@
+<?php
+    $name = "Wutipong";
+    $department = "Business Computer";
+
+    echo $name ." ". $department."<br>";
+
+    $num1 = 100;
+    $num2 = 200;
+    $num3 = 300;
+    $num4 = 400;
+
+    $sum = $num1 + $num2 + $num3 + $num4;
+    echo "sum = ".$sum."<br>";
+    $avg  = $sum / 4;
+    echo "avg = ".$avg."<br>";
+
+    #x++ คือการที่ x+1 จากนั้นเก็บค่าใน x เหมิอนเดิม
+    $x = 5;
+    echo"x= ".$x."<br>";
+    $x++;
+    echo"x= ".$x."<br>";
+
+
